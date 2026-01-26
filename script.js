b9ac994bea24b839020d3e7d3e1d7a18ac2c5218
dinellipaulo <pgsd1453@gmail.com>
2026-01-26 16:40:41 -0400

    implemented form + add book to library

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 function main(){
     const myLibrary = [];
 
-    function Book(title, author, pages, hasRead){
+    const addBtn = document.querySelector('#addBtn');
+    const titleInput = document.querySelector('#titleInput');
+    const authorInput = document.querySelector('#authorInput');
+    const pagesInput = document.querySelector('#pagesInput');
+
+    function Book(title, author, pages, hasRead){ // Constructor
         if(!new.target){
             throw Error('Use the "new" operator to call this Constructor method!'); // Prevents calling the constructor like a normal method.
         }
-
+        
+        this.id = crypto.randomUUID()
         this.title = title;
         this.author = author;
         this.pages = pages;
@@ -13,10 +19,28 @@ function main(){
         hasRead ? this.hasRead = 'has been read' : this.hasRead = 'not read yet';
     }
 
-    function addBookToLibrary(title, author, pages, hasRead){
-        const book = new Book(title, author, pages, hasRead);
+    function addBookToLibrary(book){
+        if(!titleInput.value && !authorInput.value && !pagesInput.value){
+            alert('Please, fill all fields');
+            return;
+        }
         myLibrary.push(book);
     }
+
+    function displayBooks() {
+        const bookDiv = document.querySelector('#bookDiv');
+        myLibrary.forEach( book => bookDiv.innerHTML += `${book}<br>`);
+    }
+
+    addBtn.addEventListener('click', e => {
+        e.preventDefault();
+        const book = new Book(titleInput.value, authorInput.value, pagesInput.value);
+        addBookToLibrary(book);
+        console.log(myLibrary);
+    });
+
+    displayBooks();
+
 }
 
 main();
