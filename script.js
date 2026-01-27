ce869c6db16f53584edd44a65fce31e1fc0e0960
dinellipaulo <pgsd1453@gmail.com>
2026-01-27 14:34:23 -0400

    different styles for displaying books list

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function main(){
+function main() {
     const myLibrary = [];
 
     const addBtn = document.querySelector('#addBtn');
@@ -6,8 +6,8 @@ function main(){
     const authorInput = document.querySelector('#authorInput');
     const pagesInput = document.querySelector('#pagesInput');
 
-    function Book(title, author, pages, hasRead){ // Constructor
-        if(!new.target){
+    function Book(title, author, pages, hasRead) { // Constructor
+        if (!new.target) {
             throw Error('Use the "new" operator to call this Constructor method!'); // Prevents calling the constructor like a normal method.
         }
 
@@ -19,8 +19,8 @@ function main(){
         hasRead ? this.hasRead = 'has been read' : this.hasRead = 'not read yet';
     }
 
-    function addBookToLibrary(book){
-        if(!titleInput.value || !authorInput.value || !pagesInput.value){
+    function addBookToLibrary(book) {
+        if (!titleInput.value || !authorInput.value || !pagesInput.value) {
             alert('Please, fill all fields');
             return;
         }
@@ -29,15 +29,18 @@ function main(){
 
     function displayBooks() {
         const bookDiv = document.querySelector('#bookDiv');
-        if(!bookDiv) return;
+        if (!bookDiv) return;
         bookDiv.innerHTML = '';
-        myLibrary.forEach( book => bookDiv.innerHTML += `<div>
+        myLibrary.forEach(book => {
+            bookDiv.innerHTML += `<div>
                     <h3>${book.title}</h3>
                     <p>Author: ${book.author}</p>
                     <p>Pages: ${book.pages}</p>
                     <p>Status: ${book.hasRead}</p>
                 </div>`
-            );
+        });
+        if(bookDiv.innerHTML !== '')
+            bookDiv.style.display = 'flex';
     }
 
     addBtn.addEventListener('click', e => {
